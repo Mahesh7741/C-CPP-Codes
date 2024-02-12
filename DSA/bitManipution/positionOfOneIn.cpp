0c4ef8e178a5df22035be7e08beb1ef737c8ea1f
Mahesh Savant <smahesh7741@gmail.com>
2024-02-13 01:39:22 +0530

    code of position of 1 in bit

---
+++ DSA/bitManipution/positionOfOneIn.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int positionOfOneInbit(int num){
+    if(num&1){
+        return 1;
+    }
+
+    num=num^(num&(num-1));
+    int pos=0;
+    while(num){
+        num=num>>1;
+        pos++;
+    }
+    return pos;
+}
+
+int main(){
+    int num=8;
+    int result=positionOfOneInbit(num);
+    cout<<result<<endl;
+
+}
