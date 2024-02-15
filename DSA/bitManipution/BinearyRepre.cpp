66de0835e61e25725c2b0a35bc41e61354d6b049
Mahesh Savant <smahesh7741@gmail.com>
2024-02-15 23:29:38 +0530

    Check Update of file

--- DSA/bitManipution/BinearyRepre.cpp
+++ DSA/bitManipution/BinearyRepre.cpp
@@ -1,18 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bin(long n){
-    long i;
-    cout<<"0";
-    for(i=1<<30;i>0;i=i/2){
-        if((n&i)!=0){
-            cout<<"1";
-        }else{
-            cout<<"0";
-        }
-    }
-}
 
+//Method 1: Iterative 
+
+
+// void bin(long n){
+//     long i;
+//     cout<<"0";
+//     for(i=1<<30;i>0;i=i/2){
+//         if((n&i)!=0){
+//             cout<<"1";
+//         }else{
+//             cout<<"0";
+//         }
+//     }
+// }
+
+
+// int main(){
+//     bin(7);
+// }
+
+//Method 2: Recursive 
+
+void bin(unsigned n){
+    if(n>0){
+        bin(n/2);
+    }
+    cout<<n%2;
+}
 
 int main(){
     bin(7);
